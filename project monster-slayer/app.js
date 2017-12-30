3b1afe8ebe87de8f3fa573aa63ce096a41fa89fc
andreybleme <andreybleme1@gmail.com>
2017-12-30 15:35:43 -0200

    monster slayer - creating newGame method

--- project monster-slayer/app.js
+++ project monster-slayer/app.js
@@ -3,6 +3,13 @@ new Vue({
 	data: {
 		playerHealth: 100,
 		monsterHealth: 100,
-		isGameRunning: false
+		gameIsRunning: false
+	},
+	methods: {
+		startNewGame: function () {
+			this.gameIsRunning = true;
+			this.playerHealth = 100;
+			this.monsterHealth = 100;
+		}
 	}
 });
