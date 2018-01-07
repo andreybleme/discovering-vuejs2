4c3123d21b6b4c2f9bc80d09bb369cf2a00c1963
andreybleme <andreybleme1@gmail.com>
2018-01-07 11:09:49 -0200

    communicating between components - practice, sharing data using props

---
+++ communicating-between-components/practicing/servers/src/components/Server/Server.vue
@@ -0,0 +1,13 @@
+<template>
+    <li class="list-group-item">
+        Server #{{ id }}
+    </li>
+</template>
+
+<script type="text/javascript">
+export default {
+	props: ['id']
+}
+</script>
+
+<style type="text/css"></style>
